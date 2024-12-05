0907ebeecfc99cb63abe7b74e5159f6af1fafef8
gaurav8180 <****>
2024-12-05 12:42:57 +0530

    app.py file

--- backend/app.py
+++ backend/app.py
@@ -109,4 +109,4 @@ def request_entity_too_large(error):
     return jsonify({"error": "File too large"}), 413
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
